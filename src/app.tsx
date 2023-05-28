d80cf5f2ba9472cfc74fc80a106a75f5fcd9c1bf
Julian <julianmartinez_93@hotmail.com>
2023-05-28 19:13:11 -0300

    add tailwind and flowbite

--- src/app.tsx
+++ src/app.tsx
@@ -1,42 +1,10 @@
-import { useState } from 'react'
-import { ReactComponent as Logo } from './logo.svg'
-import './app.css'
-
+import {} from 'flowbite'
 export function App () {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Logo className='App-logo' title='logo' />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type='button' onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className='App-link'
-            href='https://vitejs.dev/guide/features.html'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Vite Docs
-          </a>
-        </p>
+    <div>
+      <header className='flex flex-col items-center justify-center min-h-screen text-white'>
+        <h1 className='text-white'>Hello Word</h1>
+        <button type='button' className='focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900'>Red</button>
       </header>
     </div>
   )
