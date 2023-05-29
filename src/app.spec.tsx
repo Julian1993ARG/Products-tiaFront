ac8bf02cc0064d4fa346e03ff78d99f0267ae292
Julian <julianmartinez_93@hotmail.com>
2023-05-29 00:00:42 -0300

    add semicolon

--- src/app.spec.tsx
+++ src/app.spec.tsx
@@ -1,9 +1,9 @@
-import { Home } from '@/pages/home'
-import { render, screen } from '@testing-library/react'
+import { Home } from '@/pages/home';
+import { render, screen } from '@testing-library/react';
 
 it('Test', () => {
-  render(<Home />)
+  render(<Home />);
 
-  const button = screen.getByRole('button')
-  expect(button).toBeEnabled()
-})
+  const button = screen.getByRole('button');
+  expect(button).toBeEnabled();
+});
